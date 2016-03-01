9e431156a0a542df4ef697288583aa6532f69ba4
MadisonIkeda <maikeda@uw.edu>
2016-02-29 23:09:39 -0800

    netflix working

--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/MainActivity.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/MainActivity.java
@@ -3,11 +3,13 @@ package edu.uw.nmcgov.recommendme;
 import android.app.DownloadManager;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void goToSpotify(View v) {
+
+    // intent shows the options the user can go to in order to listen to music
+    // currently if they click on a button in the ui, different options will pop up
+    public void goToMusic(View v) {
         Log.v(TAG, "sending spotify intent");
         String artist = "Beyonce"; // hard coded. lol
 
@@ -31,6 +36,38 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(SearchManager.QUERY, artist);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        }
+    }
+
+    // currently if they press a button in the ui, then they will go to netflix for a show that doesn't exist
+    public void goToNetflix(View v) {
+        Log.v(TAG, "sending netflix intent");
+        String netFlixId = "43598743"; // <== isn't a real movie id
+        String watchUrl = "http://www.netflix.com/watch/"+netFlixId;
+
+        try {
+            // this is if you have the movie ID
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setClassName("com.netflix.mediaclient", "com.netflix.mediaclient.ui.launch.UIWebViewActivity");
+//            intent.setData(Uri.parse(watchUrl));
+//            startActivity(intent);
+
+            // I think this will work if the user types in a movie
+            String movieTitle = "Scandal";
+            
+            Intent intent = new Intent(Intent.ACTION_SEARCH);
+            intent.setClassName("com.netflix.mediaclient", "com.netflix.mediaclient.ui.search.SearchActivity");
+            intent.putExtra("query", movieTitle);
+            startActivity(intent);
+        }
+        catch(Exception e)
+        {
+            // netflix app isn't installed, send to website.
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(watchUrl));
+            startActivity(intent);
+            //Toast.makeText(this, "Please install the NetFlix App!", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
